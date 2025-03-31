905a3ae82e255fcfa9ff474b472294f7773280a7
ayhem8hamdi <ayhemhamdi24@gmail.com>
2025-04-01 01:54:16 +0200

    Preparing Empty Layouts (mobile , tablet and desktop)

--- lib/feautures/home_feature/presentation/views/widgets/dashboard_screen_body.dart
+++ lib/feautures/home_feature/presentation/views/widgets/dashboard_screen_body.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_dash_board/feautures/home_feature/presentation/views/widgets/adaptive_layout_widget.dart';
+import 'package:responsive_dash_board/feautures/home_feature/presentation/views/widgets/desktop_layout.dart';
+import 'package:responsive_dash_board/feautures/home_feature/presentation/views/widgets/mobile_layout.dart';
+import 'package:responsive_dash_board/feautures/home_feature/presentation/views/widgets/tablet_layout.dart';
 
 class DashboardScreenBody extends StatelessWidget {
   const DashboardScreenBody({super.key});
-
+  //lazy building for layouts .
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return AdaptiveLayoutWidget(
+        mobileLayout: (context) => const MobileLayout(),
+        tabletLayout: (context) => const TabletLayout(),
+        desktopLayout: (context) => const DesktopLayout());
   }
 }
