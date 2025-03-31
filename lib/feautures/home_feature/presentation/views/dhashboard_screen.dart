12996751f3c192b1d449da645ba96dc5606798db
ayhem8hamdi <ayhemhamdi24@gmail.com>
2025-04-01 01:36:22 +0200

    Set up MVVM architecture

---
+++ lib/feautures/home_feature/presentation/views/dhashboard_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_dash_board/feautures/home_feature/presentation/views/widgets/dashboard_screen_body.dart';
+
+class Dhashboardscreen extends StatelessWidget {
+  const Dhashboardscreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: DashboardScreenBody(),
+    );
+  }
+}
