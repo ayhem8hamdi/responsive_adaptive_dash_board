71eb88e57cda97a7bc9d1708db64cc341f1b246b
ayhem8hamdi <ayhemhamdi24@gmail.com>
2025-04-01 03:06:24 +0200

    Refactor AppStyle class

--- lib/core/utils/app_styles.dart
+++ lib/core/utils/app_styles.dart
@@ -3,53 +3,63 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:responsive_dash_board/core/utils/app_colors.dart';
 
 abstract class AppStyles {
-  static TextStyle styleRegular16 = GoogleFonts.montserrat(
+  static final TextStyle baseMontserrat = GoogleFonts.montserrat();
+
+  static TextStyle styleRegular16 = baseMontserrat.copyWith(
     color: AppColors.kPrimaryColor,
     fontSize: 16,
     fontWeight: FontWeight.w400,
   );
-  static TextStyle styleMeduim16 = GoogleFonts.montserrat(
+
+  static TextStyle styleMedium16 = baseMontserrat.copyWith(
     color: AppColors.kPrimaryColor,
     fontSize: 16,
     fontWeight: FontWeight.w500,
   );
-  static TextStyle styleSemiBold16 = GoogleFonts.montserrat(
+
+  static TextStyle styleSemiBold16 = baseMontserrat.copyWith(
     color: AppColors.kPrimaryColor,
     fontSize: 16,
     fontWeight: FontWeight.w600,
   );
 
-  static TextStyle styleSemiBold20 = GoogleFonts.montserrat(
+  static TextStyle styleSemiBold20 = baseMontserrat.copyWith(
     color: AppColors.kPrimaryColor,
     fontSize: 20,
     fontWeight: FontWeight.w600,
   );
-  static TextStyle styleRegular12 = GoogleFonts.montserrat(
+
+  static TextStyle styleRegular12 = baseMontserrat.copyWith(
     color: AppColors.kThirdColor,
     fontSize: 12,
     fontWeight: FontWeight.w400,
   );
-  static TextStyle styleSemiBold24 = GoogleFonts.montserrat(
+
+  static TextStyle styleSemiBold24 = baseMontserrat.copyWith(
     color: AppColors.kSecondaryColor,
     fontSize: 24,
     fontWeight: FontWeight.w600,
   );
-  static TextStyle styleRegular14 = GoogleFonts.montserrat(
+
+  static TextStyle styleRegular14 = baseMontserrat.copyWith(
     color: AppColors.kThirdColor,
     fontSize: 14,
     fontWeight: FontWeight.w400,
   );
-  static TextStyle styleSemiBold18 = GoogleFonts.montserrat(
+
+  static TextStyle styleSemiBold18 = baseMontserrat.copyWith(
     color: AppColors.kSecondaryColor,
     fontSize: 18,
     fontWeight: FontWeight.w600,
   );
-  static TextStyle styleBold16 = GoogleFonts.montserrat(
+
+  static TextStyle styleBold16 = baseMontserrat.copyWith(
     color: AppColors.kSecondaryColor,
     fontSize: 16,
     fontWeight: FontWeight.w700,
   );
-  static TextStyle styleMeduim20 = GoogleFonts.montserrat(
+
+  static TextStyle styleMedium20 = baseMontserrat.copyWith(
     color: AppColors.kThirdColor,
     fontSize: 20,
     fontWeight: FontWeight.w500,
