c4fd16921f916332b999579375e21a88e4aa450b
ayhem8hamdi <ayhemhamdi24@gmail.com>
2025-04-02 23:28:21 +0100

    Fix scrollable drawer problem

--- lib/feautures/home_feature/presentation/views/widgets/custom_drawer.dart
+++ lib/feautures/home_feature/presentation/views/widgets/custom_drawer.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_dash_board/core/utils/app_images.dart';
+import 'package:responsive_dash_board/feautures/home_feature/data/models/drawer_item_model.dart';
 import 'package:responsive_dash_board/feautures/home_feature/data/models/user_model.dart';
+import 'package:responsive_dash_board/feautures/home_feature/presentation/views/widgets/custom_drawer_item.dart';
 import 'package:responsive_dash_board/feautures/home_feature/presentation/views/widgets/custom_user_list_tile.dart';
 import 'package:responsive_dash_board/feautures/home_feature/presentation/views/widgets/items_list.dart';
 
@@ -11,16 +13,38 @@ class CustomDrawer extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       color: Colors.white,
-      child: const Column(
-        children: [
-          CustomUserListTile(
-            user: UserModel(
-                image: AppAssets.avatar1,
-                userName: 'Ayhem Hamdi',
-                userMail: 'ayhemhamdi24@gmail.com'),
+      child: const CustomScrollView(
+        slivers: [
+          SliverToBoxAdapter(
+            child: CustomUserListTile(
+              user: UserModel(
+                  image: AppAssets.avatar1,
+                  userName: 'Ayhem Hamdi',
+                  userMail: 'ayhemhamdi24@gmail.com'),
+            ),
           ),
-          SizedBox(height: 8),
-          ItemsList()
+          SliverToBoxAdapter(child: SizedBox(height: 8)),
+          ItemsList(),
+          SliverFillRemaining(
+            hasScrollBody: false,
+            fillOverscroll: false,
+            child: Column(
+              children: [
+                Expanded(child: SizedBox()),
+                InactiveListTile(
+                  item: DrawerItemModel(
+                      image: AppAssets.settings, title: 'Settings'),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(bottom: 20),
+                  child: InactiveListTile(
+                    item: DrawerItemModel(
+                        image: AppAssets.logout, title: 'LogOut'),
+                  ),
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
--- lib/feautures/home_feature/presentation/views/widgets/items_list.dart
+++ lib/feautures/home_feature/presentation/views/widgets/items_list.dart
@@ -22,9 +22,7 @@ class _ItemsListState extends State<ItemsList> {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-        shrinkWrap: true,
-        physics: const NeverScrollableScrollPhysics(),
+    return SliverList.builder(
         itemCount: items.length,
         itemBuilder: (context, index) => GestureDetector(
               onTap: () {
