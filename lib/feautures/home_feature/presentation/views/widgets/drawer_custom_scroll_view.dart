0701890a542f1fed17cb3385e46bb9f883e4f2e2
ayhem8hamdi <ayhemhamdi24@gmail.com>
2025-04-02 23:44:03 +0100

    Custom drawer refactor

---
+++ lib/feautures/home_feature/presentation/views/widgets/drawer_custom_scroll_view.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_dash_board/core/utils/app_images.dart';
+import 'package:responsive_dash_board/feautures/home_feature/data/models/drawer_item_model.dart';
+import 'package:responsive_dash_board/feautures/home_feature/data/models/user_model.dart';
+import 'package:responsive_dash_board/feautures/home_feature/presentation/views/widgets/custom_drawer_item.dart';
+import 'package:responsive_dash_board/feautures/home_feature/presentation/views/widgets/custom_user_list_tile.dart';
+import 'package:responsive_dash_board/feautures/home_feature/presentation/views/widgets/items_list.dart';
+
+class DrawerCustomScrollView extends StatelessWidget {
+  const DrawerCustomScrollView({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const CustomScrollView(
+      slivers: [
+        SliverToBoxAdapter(
+          child: CustomUserListTile(
+            user: UserModel(
+                image: AppAssets.avatar1,
+                userName: 'Ayhem Hamdi',
+                userMail: 'ayhemhamdi24@gmail.com'),
+          ),
+        ),
+        SliverToBoxAdapter(child: SizedBox(height: 8)),
+        ItemsList(),
+        SliverFillRemaining(
+          hasScrollBody: false,
+          fillOverscroll: false,
+          child: Column(
+            children: [
+              Expanded(
+                  child: SizedBox(
+                height: 20,
+              )),
+              InactiveListTile(
+                item: DrawerItemModel(
+                    image: AppAssets.settings, title: 'Settings'),
+              ),
+              Padding(
+                padding: EdgeInsets.only(bottom: 20),
+                child: InactiveListTile(
+                  item:
+                      DrawerItemModel(image: AppAssets.logout, title: 'LogOut'),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
