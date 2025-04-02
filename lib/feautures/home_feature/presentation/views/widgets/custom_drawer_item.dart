21a4cc2ecd86fc82d6c2261375dc189b972441af
ayhem8hamdi <ayhemhamdi24@gmail.com>
2025-04-02 14:13:09 +0100

    change items state

--- lib/feautures/home_feature/presentation/views/widgets/custom_drawer_item.dart
+++ lib/feautures/home_feature/presentation/views/widgets/custom_drawer_item.dart
@@ -1,11 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:responsive_dash_board/core/utils/app_colors.dart';
 import 'package:responsive_dash_board/core/utils/app_styles.dart';
 import 'package:responsive_dash_board/feautures/home_feature/data/models/drawer_item_model.dart';
 
 class CustomDrawerItem extends StatelessWidget {
-  const CustomDrawerItem({super.key, required this.item});
+  const CustomDrawerItem(
+      {super.key, required this.item, required this.isActive});
   final DrawerItemModel item;
+  final bool isActive;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 20),
+      child:
+          isActive ? ActiveListTile(item: item) : InactiveListTile(item: item),
+    );
+  }
+}
+
+class InactiveListTile extends StatelessWidget {
+  const InactiveListTile({
+    super.key,
+    required this.item,
+  });
+
+  final DrawerItemModel item;
+
   @override
   Widget build(BuildContext context) {
     return ListTile(
@@ -13,6 +34,30 @@ class CustomDrawerItem extends StatelessWidget {
       title: Text(
         item.title,
         style: AppStyles.styleRegular16,
+      ),
+    );
+  }
+}
+
+class ActiveListTile extends StatelessWidget {
+  const ActiveListTile({
+    super.key,
+    required this.item,
+  });
+
+  final DrawerItemModel item;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      trailing: Container(
+        width: 4,
+        color: AppColors.kSecondaryColor,
+      ),
+      leading: SvgPicture.asset(item.image),
+      title: Text(
+        item.title,
+        style: AppStyles.styleBold16,
       ),
     );
   }
--- lib/feautures/home_feature/presentation/views/widgets/items_list.dart
+++ lib/feautures/home_feature/presentation/views/widgets/items_list.dart
@@ -3,21 +3,39 @@ import 'package:responsive_dash_board/core/utils/app_images.dart';
 import 'package:responsive_dash_board/feautures/home_feature/data/models/drawer_item_model.dart';
 import 'package:responsive_dash_board/feautures/home_feature/presentation/views/widgets/custom_drawer_item.dart';
 
-class ItemsList extends StatelessWidget {
+class ItemsList extends StatefulWidget {
   const ItemsList({super.key});
-  static const List<DrawerItemModel> items = [
+
+  @override
+  State<ItemsList> createState() => _ItemsListState();
+}
+
+class _ItemsListState extends State<ItemsList> {
+  int activeIndex = -1;
+  final List<DrawerItemModel> items = const [
     DrawerItemModel(image: AppAssets.dashboard, title: 'Dashboard'),
     DrawerItemModel(image: AppAssets.transaction, title: 'My Transaction'),
     DrawerItemModel(image: AppAssets.statistics, title: 'Statistics'),
     DrawerItemModel(image: AppAssets.wallet, title: 'Wallet Account'),
     DrawerItemModel(image: AppAssets.investment, title: 'My Investments'),
   ];
+
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
         shrinkWrap: true,
         physics: const NeverScrollableScrollPhysics(),
         itemCount: items.length,
-        itemBuilder: (context, index) => CustomDrawerItem(item: items[index]));
+        itemBuilder: (context, index) => GestureDetector(
+              onTap: () {
+                setState(() {
+                  activeIndex = index;
+                });
+              },
+              child: CustomDrawerItem(
+                item: items[index],
+                isActive: activeIndex == index,
+              ),
+            ));
   }
 }
