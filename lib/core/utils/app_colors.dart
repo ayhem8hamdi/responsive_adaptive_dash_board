49e1f561b3cbd04be86a4de84f32318fc64fad16
ayhem8hamdi <ayhemhamdi24@gmail.com>
2025-04-03 00:59:08 +0100

    Create All Expanses Item part 1

--- lib/core/utils/app_colors.dart
+++ lib/core/utils/app_colors.dart
@@ -3,5 +3,5 @@ import 'package:flutter/material.dart';
 abstract class AppColors {
   static const Color kPrimaryColor = Color(0xFF064061);
   static const Color kSecondaryColor = Color(0xFF4EB7F2);
-  static const Color kThirdColor = Color(0xffAAAAAA);
+  static const Color kThirdColor = Color(0xFFAAAAAA);
 }
--- lib/feautures/home_feature/presentation/views/widgets/all_expanses_widget.dart
+++ lib/feautures/home_feature/presentation/views/widgets/all_expanses_widget.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:responsive_dash_board/core/utils/app_colors.dart';
+import 'package:responsive_dash_board/core/utils/app_images.dart';
 import 'package:responsive_dash_board/feautures/home_feature/presentation/views/widgets/all_expanses_header.dart';
 
 class AllExpansesWidget extends StatelessWidget {
@@ -14,8 +17,80 @@ class AllExpansesWidget extends StatelessWidget {
         color: Colors.white,
       ),
       child: const Column(
-        children: [AllExpansesHeader()],
+        children: [AllExpansesHeader(), AllExpansesCard()],
       ),
     );
+  }
+}
+
+class AllExpansesCard extends StatelessWidget {
+  const AllExpansesCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        AllExpansesCustomCard(),
+        SizedBox(
+          width: 10,
+        ),
+        AllExpansesCustomCard(),
+        SizedBox(
+          width: 10,
+        ),
+        AllExpansesCustomCard()
+      ],
+    );
+  }
+}
+
+class AllExpansesCustomCard extends StatelessWidget {
+  const AllExpansesCustomCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+        child: Container(
+      padding: const EdgeInsets.all(12),
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(12),
+          border: Border.all(
+            width: 1,
+            color: const Color(0xFFF1F1F1),
+          )),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            children: [
+              Container(
+                  padding: EdgeInsets.all(9),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(120),
+                    color: const Color(0xFFAAAAAA),
+                  ),
+                  child: SvgPicture.asset(AppAssets.income)),
+              const Spacer(),
+              Transform.rotate(
+                  angle: 3.1416,
+                  child: Icon(
+                    Icons.arrow_back_ios_new_outlined,
+                    color: AppColors.kPrimaryColor,
+                  ))
+            ],
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Text('balance'),
+          SizedBox(height: 5),
+          Text('April 2022'),
+          SizedBox(
+            height: 9,
+          ),
+          Text('\$20,129')
+        ],
+      ),
+    ));
   }
 }
