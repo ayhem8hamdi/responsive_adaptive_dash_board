6b080d7e67f8eb747e4f914292aac6ba8075e76d
ayhem8hamdi <ayhemhamdi24@gmail.com>
2025-05-31 17:13:17 +0100

    Done Quick Invoice Button Item

--- lib/feautures/home_feature/presentation/views/widgets/quick_invoice_buttons_section.dart
+++ lib/feautures/home_feature/presentation/views/widgets/quick_invoice_buttons_section.dart
@@ -1,19 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_dash_board/core/utils/app_styles.dart';
 
 class QuickInvoiceButtonsSection extends StatelessWidget {
   const QuickInvoiceButtonsSection({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Row(
+      children: [],
+    );
   }
 }
 
 class QuickInvoiceButtonItem extends StatelessWidget {
-  const QuickInvoiceButtonItem({super.key});
-
+  const QuickInvoiceButtonItem(
+      {super.key, required this.color, required this.text});
+  final Color color;
+  final String text;
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+      padding: const EdgeInsets.all(20),
+      decoration:
+          BoxDecoration(color: color, borderRadius: BorderRadius.circular(12)),
+      child: Text(
+        text,
+        style: AppStyles.styleSemiBold18,
+      ),
+    );
   }
 }
